70f6086f8d503b98cf9326c27ff7e4dc5052942d
Linqing <62589604+Lanceart@users.noreply.github.com>
2024-06-30 11:36:17 -0700

    step: cors

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,12 @@ app.use(cors({
 }));
 
 
-app.options('*', cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 //Routes
