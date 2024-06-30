3dba5ce778237d46f2c9408a5f1114a6fd90a853
Khang M Bui <khangbui2002@gmail.com>
2024-06-29 17:22:10 -0700

    Finished welcome and desktop page.

---
+++ frontend/app/welcome/page.js
@@ -0,0 +1,23 @@
+"use client";
+import React, { useState, useEffect } from 'react';
+import Welcome from '@/components/Welcome';
+import { isMobile } from 'react-device-detect';
+import Desktop from '@/components/Desktop';
+
+export default function Page() {
+  const [isClient, setIsClient] = useState(false);
+  const [clientIsMobile, setClientIsMobile] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+    setClientIsMobile(isMobile);
+  }, []);
+
+  console.log(clientIsMobile, isMobile, "what");
+
+  return (
+    <div>
+      {isClient ? (clientIsMobile ? <Welcome /> : <Desktop />) : null}
+    </div>
+  );
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,29 @@ module.exports = {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        'strong': '0 10px 15px rgba(0, 0, 0, 0.5)', 
+      },
+      fontFamily: {
+      'sans': ['Helvetica', 'Arial', 'sans-serif'],
+      'serif': ['Georgia', 'serif'],
+      'Lato': ['Lato'],
+      },
+      colors: {
+        'darkblue' : {
+        200: '#2E6171', // Lighter shade of your darkblue color
+        DEFAULT: '#013C4E', // Default shade of darkblue
+        },
+        'headerBlue' : '#72F6FD',
+        'cyan'  : '#72F6FD',  
+        'white': '#ffffff',
+        'inputGray': {
+          DEFAULT: '#D9D9D9',
+          200: '#818589',
+        },
+        'welcomeGreen': '#01D100',
+        'signupGray': '#9F9F9F',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
