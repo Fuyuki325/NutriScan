7fa3ff94722085b7290dacf66a260b9b80d841b5
Khang M Bui <khangbui2002@gmail.com>
2024-06-29 17:32:12 -0700

    Initialized the LongButton and the Login page.

---
+++ frontend/components/Login.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import LongButton from '@/components/LongButton';
+import Link from 'next/link';
+import { isMobile } from 'react-device-detect';
+import { useRouter } from 'next/navigation'
+
+const Login = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3001/api/users/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Login failed');
+      }
+
+      const data = await response.json();
+      setCookie('sessionID', data.sessionID)
+
+      router.push('/scan');
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    // Check if sessionID cookie exists
+    const sessionID = document.cookie.split('; ').find(row => row.startsWith('sessionID='));
+    if (sessionID) {
+      console.log('SessionID cookie found:', sessionID);
+      router.push('/scan');
+    } else {
+      console.log('SessionID cookie not found');
+    }
+  }, []); // Empty dependency array to run this effect only once on component mount
+  
+  return (
+    <>
+      <title>Login</title>
+      <h1 className="text-headerBlue font-Lato text-5xl font-bold w-199 h-77 mb-16 mt-12 ml-8">LOGIN</h1>
+      <div className="flex flex-col items-center">
+        {error && <p className="text-red-500 mb-4">{error}</p>}
+        <form onSubmit={handleSubmit} className="w-5/6">
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            className="border-b-2 border-inputGray focus:border-inputGray-200 w-full py-2 px-3 leading-tight focus:outline-none mb-6"
+            placeholder="Email"
+            required
+          />
+          <input 
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            className="border-b-2 border-inputGray focus:border-inputGray-200 w-full py-2 px-3 leading-tight focus:outline-none mb-6"
+            placeholder="Password"
+            required
+          />
+          <div className='flex flex-row justify-center'>
+            <LongButton text="LOGIN" />
+          </div>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default Login;
